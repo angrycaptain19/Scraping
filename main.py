f18f6e36200fbc73021969d2dcf740be98f3b5b8
ViktorKucherov <viktorkucherov@outlook.com>
2021-02-28 14:31:51 +0300

    2 commit

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from bs4 import BeautifulSoup as bs4
 
+
+def getSite(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    except URLError as e:
+        return None
+
+    try:
+        bs = bs4(html, 'html.parser')
+    except AttributeError as e:
+        return None
+
+    return bs
+
+site = getSite('https://www.moex.com/')
+print(site.find_all('div')[4].find_all('img'))
